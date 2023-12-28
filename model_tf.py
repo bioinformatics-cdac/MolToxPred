feaf80cc4ba88a4681b4d4f26986c66b920208a3
bioinformatics-cdac <65070721+bioinformatics-cdac@users.noreply.github.com>
2023-12-28 14:05:04 +0530

    model_tf.py

--- model_tf.py
+++ model_tf.py
@@ -1,15 +1,23 @@
 
-# tensorflow model
+import tensorflow as tf
+# Define the TensorFlow model
 def create_tf_model():
+    tf.random.set_seed(89)
     model = tf.keras.Sequential([
-        tf.keras.layers.Dense(2000, activation='relu', input_shape=(X_train.shape[1],), kernel_regularizer=tf.keras.regularizers.l2(0.01)),
-        tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(1000, activation='relu'),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(500, activation='relu'),
-        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(1024, activation='relu', input_shape=(2313,),
+                              kernel_regularizer=tf.keras.regularizers.l2(0.0069)),
+        tf.keras.layers.Dropout(0.37, seed=12),
+        tf.keras.layers.Dense(256, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.0069)),
+        tf.keras.layers.Dropout(0.37, seed=12),
+        tf.keras.layers.Dense(64, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.0069)),
+        tf.keras.layers.Dropout(0.37, seed=12),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    # Create the optimizer with the desired learning rate
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.00126)
+
+    # Compile the model with the optimizer
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
